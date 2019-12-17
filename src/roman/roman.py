c97a221e5f406b5cf06df849697fac5b9d82a82d
Olly Shaw <ollyjshaw@gmail.com>
2019-12-17 08:01:36 +0000

    reorder

--- src/roman/roman.py
+++ src/roman/roman.py
@@ -1,13 +1,14 @@
 def roman_convert(number_to_convert):
     all_i = "I" * number_to_convert
     all_i = all_i.replace("IIIIIIIIII", "X")
-    all_i = all_i.replace("XXXXXXXXXX", "C")
-    all_i = all_i.replace("CCCCC", "D")
-    all_i = all_i.replace("CCCC", "CD")
-    all_i = all_i.replace("XXXXXXXXX", "XC")
-    all_i = all_i.replace("XXXXX", "L")
-    all_i = all_i.replace("XXXX", "XL")
     all_i = all_i.replace("IIIIIIIII", "IX")
     all_i = all_i.replace("IIIII", "V")
     all_i = all_i.replace("IIII", "IV")
+    all_i = all_i.replace("XXXXXXXXXX", "C")
+    all_i = all_i.replace("XXXXXXXXX", "XC")
+    all_i = all_i.replace("XXXXX", "L")
+    all_i = all_i.replace("XXXX", "XL")
+    all_i = all_i.replace("CCCCC", "D")
+    all_i = all_i.replace("CCCC", "CD")
+
     return all_i
